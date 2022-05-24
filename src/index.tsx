2e88eacb134fac76b7430b9fba5475619254a037
Saerom Park <park.srome@gmail.com>
2022-05-24 06:35:10 -0500

    pwa init

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { HashRouter as Router } from 'react-router-dom';
 
 import App from 'src/App';
 import reportWebVitals from 'src/reportWebVitals';
+import * as serviceWorkerRegistration from 'src/serviceWorkerRegistration';
 
 import './index.scss';
 
@@ -15,6 +16,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
